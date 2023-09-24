e4a98f006956656b6f0a8c5f0e2f5547c4e681d9
mateuskleinwilmsen <oisouumapessoa90@gmail.com>
2023-09-24 20:22:36 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function telaEntrar(){
+    var divAviso = document.getElementById("aviso");
+    divAviso.style.left = "-50%";
+}
+
+function telaCriar(){
+    var divAviso = document.getElementById("aviso");
+    divAviso.style.left = "50%";
+}
