4fdf1b542907a2300c19beff35662ed3be59a7f4
mateuskleinwilmsen <oisouumapessoa90@gmail.com>
2023-09-24 21:32:54 -0300

    fazendo o site ficar mais responsivo

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
 function telaEntrar(){
-    var divAviso = document.getElementById("aviso");
-    divAviso.style.left = "-50%";
+    let divAviso = document.getElementById("aviso");
+    let divCriarConta = document.getElementById("criar-conta");
+    let width = window.outerWidth;
+
+    if(width<768){
+        divAviso.style.left = "-100%";
+    }else{divAviso.style.left = "-50%";}
+    divCriarConta.style.zIndex = "0";
 }
 
 function telaCriar(){
-    var divAviso = document.getElementById("aviso");
-    divAviso.style.left = "50%";
+    let divAviso = document.getElementById("aviso");
+    let divCriarConta = document.getElementById("criar-conta");
+    let width = window.outerWidth;
+
+    if(width<768){
+        divAviso.style.left = "0%";
+    }else{divAviso.style.left = "50%";}
+    divCriarConta.style.zIndex = "2";
 }
+
+//função que é executada toda vez que a tela muda de tamanho
+function displayWindowSize(){
+    let divAviso = document.getElementById("aviso");
+    let divCriarConta = document.getElementById("criar-conta");
+    let width = window.outerWidth;
+    
+    if(width<768){
+        divAviso.style.left = "0%";
+    }else{
+        divAviso.style.left = "50%";
+    }
+    divCriarConta.style.zIndex = "2";
+}
+    
+//evento que chama a função quando a tela muda de tamanho
+window.addEventListener("resize", displayWindowSize);
+
+displayWindowSize();
